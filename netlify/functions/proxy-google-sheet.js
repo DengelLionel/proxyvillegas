34e9f69494ee2ad4d0898c6c8d060f04633e46c0
DengelLionel <dengelriveracajincho@gmail.com>
2024-10-28 20:58:22 -0500

    ok

--- netlify/functions/proxy-google-sheet.js
+++ netlify/functions/proxy-google-sheet.js
@@ -1,8 +1,20 @@
 const axios = require('axios');
 
 exports.handler = async function (event, context) {
+  // Responder a la solicitud preflight de CORS
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': 'https://metodovillegas.com',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      },
+      body: '',
+    };
+  }
+
   try {
-    // Obtener el nombre de la hoja desde los parámetros de la URL (query string)
     const sheetName = event.queryStringParameters.sheet;
 
     if (!sheetName) {
@@ -12,24 +24,21 @@ exports.handler = async function (event, context) {
       };
     }
 
-    // URL del script de Google Apps con el parámetro de hoja
     const googleSheetUrl = `https://script.google.com/macros/s/AKfycbzKw0JJcKZWC6P30YtZfH_JEkCc-Ddz4Pqi05fQHSLDj2j18p_z-z3ECxbusVl8CZiYrg/exec?sheet=${sheetName}`;
     
-    // Hacer la solicitud a Google Apps Script con el nombre de la hoja
     const response = await axios.get(googleSheetUrl);
-    console.log("Datos recibidos desde Google Apps Script:", response.data); // Log para depurar
+    console.log("Datos recibidos desde Google Apps Script:", response.data);
     
     return {
       statusCode: 200,
       headers: {
         'Access-Control-Allow-Origin': 'https://metodovillegas.com',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Content-Type': 'application/json; charset=utf-8'  // Forzar UTF-8
+        'Content-Type': 'application/json; charset=utf-8'
       },
       body: JSON.stringify(response.data),
     };
   } catch (error) {
-    console.error("Error al obtener los datos:", error); // Log de error detallado
+    console.error("Error al obtener los datos:", error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Error fetching data' }),
